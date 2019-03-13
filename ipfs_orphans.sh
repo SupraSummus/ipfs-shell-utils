82f15cbec9dbf2f3031ac89715023847ac48beb6
Jan Rydzewski <flegmer@gmail.com>
2019-03-13 23:31:44 +0100

    ipfs-orphans exits at name resolve error

--- ipfs_orphans.sh
+++ ipfs_orphans.sh
@@ -1,10 +1,10 @@
-#/bin/sh
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
 
 comm -1 -3 <(
 	ipfs key list -l | \
 	cut -d ' ' -f 1 | \
-	xargs -n 1 ipfs name resolve | \
+	xargs -n 1 sh -c 'ipfs name resolve $0 || exit 255' | \
 	sed -e 's/^\/ipfs\///' | \
 	sort
 ) <(
