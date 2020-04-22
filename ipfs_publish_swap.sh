dcac2cdb3b68b58b413101044af8c8abf55cec73
Jan Rydzewski <flegmer@gmail.com>
2020-04-22 14:58:12 +0200

    Allow passing params to `ipfs name publish` in `ipfs-publish-swap`

--- ipfs_publish_swap.sh
+++ ipfs_publish_swap.sh
@@ -1,12 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
+
+KEY=${@: -1}
+PARAMS=${@: 1:$#-1}
 
 for path in $(cat -); do
 
-	old_path=$(ipfs name resolve -n -- "$1" | cut -d / -f 3)
+	old_path=$(ipfs name resolve -n -- "$KEY" | cut -d / -f 3)
+	ipfs name publish $PARAMS --key "$KEY" -- "$path" 1>&2
 
 	if [ "$old_path" != "$path" ]; then
-		ipfs name publish --key "$1" -- "$path" 1>&2
 		echo "$old_path"
 	fi
 
